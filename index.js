318f4df6b45874074f59c626b9f3c63ac4880f87
untitledresearchlab <untitledrabbithole@gmail.com>
2023-11-10 16:46:36 +0800

    Add existing project files to Git

---
+++ index.js
@@ -0,0 +1,351 @@
+/*
+
+⠀⠀⠀⠀⠀⠀⠀⠀⣀⡤⠤⢤⣄⠀⢀⣤⠤⠤⠤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣠⠞⠉⠀⠀⠀⠈⣻⠋⠀⠀⠀⠀⠀⠉⠻⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣰⣯⡴⠖⠲⠦⣤⡴⠛⠉⠉⠉⠙⠒⢦⣄⠀⠈⠻⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣠⠞⠋⠀⠀⠀⠀⣠⢏⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⣠⣾⣿⣿⣿⣿⣄⠀⢰⢟⣩⣿⡿⠿⠿⠿⠿⠿⣷⣆⠀⠀⠀⠀⢸⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⣹⣿⠿⣾⣷⣿⣯⣡⣿⣿⠟⠛⣷⣶⣶⣶⣶⣶⡾⠿⠃⠀⠀⠀⠘⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡶⠷⢶⣤⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠻⣷⣤⣿⣾⢿⣿⣿⣿⣯⣀⣀⣿⣿⣿⣛⣩⠾⠃⠀⠀⠀⠀⠀⠀⢿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠇⠀⠀⠘⣧⠀⠀⠀⠀⠀⠀⠀
+⠀⢀⡿⢿⡇⠐⣶⠞⠋⠉⠻⣶⠒⠒⠋⠉⠉⠁⠀⠀⢠⣄⠀⠀⠀⠀⠘⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⢹⡀⠀⠀⠀⠀⠀⠀
+⢰⣿⡀⠀⠀⠞⠁⠀⠀⠀⠀⠈⠙⠶⢤⡀⠀⠀⠀⣠⣶⣶⣧⠀⠀⠀⠀⢿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀
+⠀⠻⣷⣤⣀⣀⣀⣀⣀⣀⣀⣀⣀⣤⠴⠖⣺⣿⣫⢷⣳⠿⠁⠀⠀⠀⠀⠘⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇⠀⠀⢸⣃⣼⠁⠀⠀⠀⠀⠀⠀
+⠀⠸⣏⠛⠖⠒⠒⠒⠒⠒⠚⠚⠛⠛⠛⣋⣡⠶⠚⠋⠀⠀⠀⠀⠀⠀⠀⠀⠻⣧⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡀⣾⠇⠀⠀⠈⣽⡟⣠⣾⣶⡄⠀⠀⠀
+⠀⠀⢹⡗⠶⠤⠤⠤⠤⠴⠤⠶⠖⠒⠚⠉⠁⠀⠀⠀⠀⠀⠀⣷⠀⢻⡄⠀⠀⢹⣷⠀⠀⠀⣀⣴⠶⠟⠛⠛⣻⡟⠀⠀⠀⠰⢿⠞⣛⣷⣞⣇⡀⠀⠀
+⠀⠀⠈⢻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡆⠈⣿⡀⠀⠀⣻⣧⣠⡾⠻⣥⣀⠀⣀⣴⣿⣦⣶⣶⣶⣬⣍⡈⠙⠛⠛⠛⠛⣷⡀
+⠀⠀⠀⠀⠙⢷⡀⠀⠀⠀⠀HELLO⠀⠀⠀⠀⠀⠀⠀⠀⢻⡀⢹⣇⠀⠀⠀⢿⡟⠀⣀⣈⣹⡿⠟⠁⠀⠀⠀⠀⠀⠀⠉⠻⣦⢀⡀⢰⣤⣿⠃
+⠀⠀⠀⠀⠀⠈⠻⣦⡀⠀⠀⠀THERE⠀⠀⠀⠀⠀⠀⠀⠘⣇⠀⢿⠀⠀⠀⠀⠻⠆⠀⣼⡟⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠹⣷⠿⠞⠛⠁⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠛⢷⣄⠀⠀⠀THIS⠀⠀⠀⠀⠀⠀⠀⢹⡄⢸⣇⠀⠀⠀⠀⠀⠀⠉⠀⠀⠀⠴⠟⠀⠀⠀⠀⠀⠀⠀⠀⣿⡄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣧⡀⠀⠀IS THE⠀⠀⠀⠀⠀⠈⣿⡈⢿⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⢠⡿⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣷⡀⠀⠀⠀GOOGLE⠀⠀⠀⠀⠀⢀⣾⣇⠰⣷⣤⣭⣿⠆⠀⠀⠀⠀⠀⣶⣟⢠⠀⠀⠀⠀⠀⢀⣿⠃⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣄⠀⠀APP⠀⠀⠀⠀⠀⢸⣏⢸⣧⣽⡅⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠋⢀⣀⠀⠀⣠⣾⠋⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠛⠈⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⣀⣼⡿⠃⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⢀⣤⣤⣴⡾⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣧⠀⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠻⠷⣶⣦⣤⣤⣤⣤⣤⣤⣶⣶⡶⠿⠛⠉⠀⢻⡄⠸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠁⣿⡀⠀⠀⠀⠀⣰⡶⠶⠶⣾⣿⠀⢻⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⣿⠀⠀⠀⠀⠀⠘⠿⣦⣄⡀⠀⠀⠘⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣤⣤⣄⡀⠀⣿⠀⣿⠁⠀⠀⠀⠀⠀⠀⠈⠉⠛⠳⠶⣶⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣆⡉⠛⢿⣿⢀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠿⣶⣄⣁⣸⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+[PLS CHANGE THIS]
+IP ADDRESS LIST:
+ROUTER : 192.168.0.1
+NODEJS SERVER : 192.168.0.2 / 	48-21-0B-59-24-5A
+WATCHOUT : 192.168.0.3 / B4-2E-99-F8-16-47
+MADMAPPER : 192.168.0.4 / 48-21-0B-3B-64-9B
+CAPTURE CARD 1 : 192.168.0.5 / D0-C8-57-81-01-9F
+CAPTURE CARD 2 : 192.168.0.6 / D0-C8-57-81-01-A0
+
+
+*/
+
+const express = require('express')
+const app = express()
+const port = 3000
+var udp = require('dgram');
+var cors = require('cors')
+const cron = require("node-cron");
+const axios = require('axios');
+var pjlink = require('pjlink');
+
+var client = udp.createSocket('udp4');
+
+app.use(express.static('public'));
+
+var whitelist = ['https://localhost']
+var corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors({
+    origin: 'https://192.168.0.2:3000',
+})
+);
+app.options(corsOptions, cors());
+
+//WO Display IP Address
+var PORT = 3039;
+var HOST = '192.168.1.123';
+
+//Projector IP Addressaaa
+var beamer1 = new pjlink("192.168.1.200", 4352, "123");
+var beamer2 = new pjlink("192.168.1.201", 4352, "123");
+var beamer3 = new pjlink("192.168.1.202", 4352, "123");
+var beamer4 = new pjlink("192.168.1.203", 4352, "123");
+var beamer5 = new pjlink("192.168.1.204", 4352, "123");
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/wakewo', wakeWatchout);
+app.get('/wakeprojector', wakeProjector);
+
+function wakeProjector(req, res) {
+
+    beamer1.powerOn(function (err) {
+        if (err) {
+            console.log('error turning on', err);
+            return;
+        }
+    });
+
+    beamer2.powerOn(function (err) {
+        if (err) {
+            console.log('error turning on', err);
+            return;
+        }
+    });
+
+
+    beamer3.powerOn(function (err) {
+
+        if (err) {
+
+            console.log('error turning on', err);
+            return;
+        }
+    });
+
+    beamer4.powerOn(function (err) {
+        if (err) {
+            console.log('error turning on', err);
+            return;
+        }
+    });
+
+
+    beamer5.powerOn(function (err) {
+        if (err) {
+            console.log('error turning on', err);
+            return;
+        }
+    });
+}
+
+function wakeWatchout() {
+
+    // wake on lan
+
+    var wol = require('wake_on_lan');
+
+    wol.wake('E0D55EE619BF');
+
+    wol.wake('E0D55EE619BF', function (error) {
+
+        if (error) {
+            // handle error
+        } else {
+            // done sending packets
+        }
+    });
+
+    var magic_packet = wol1.createMagicPacket('E0:D5:5E:E6:19:BF');
+
+    //.
+
+    // e0:d5:5e:e6:19:bf
+
+    console.log("System Turned On");
+
+    beamer1.powerOn(function (err) {
+
+        if (err) {
+           console.log('error turning on', err);
+            return;
+        }
+
+    });
+
+
+    beamer2.powerOn(function (err) {
+        if (err) {
+            console.log('error turning on', err);
+            return;
+        }
+    });
+
+
+
+       
+    beamer3.powerOn(function (err) {
+        if (err) {
+            console.log('error turning on', err);
+            return;
+        }
+    });
+
+    beamer4.powerOn(function (err) {
+        if (err) {
+            console.log('error turning on', err);
+            return;
+        }
+    });
+
+
+    beamer5.powerOn(function (err) {
+        if (err) {
+            console.log('error turning on', err);
+            return;
+        }
+    });
+    
+  
+}
+
+app.get('/shutwo', shutdownWatchout);
+
+var shutdownWatchout = Buffer.from("authenticate 1  \npowerDown");
+
+function shutdownWatchout(req, res) {
+    client.send(shutdownWatchout, 0, shutdownWatchout.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + HOST + ':' + PORT);
+        console.log(bytes);
+        //client.close();
+       // res.send('Send message to Watchout');
+        console.log('Shutting down Internal Display');
+    });
+
+    beamer1.powerOff(function (err) {
+        if (err) {
+            console.log('error turning off', err);
+            return;
+        }
+    });
+
+    beamer2.powerOff(function (err) {
+        if (err) {
+            console.log('error turning off', err);
+            return;
+        }
+    });
+
+    beamer3.powerOff(function (err) {
+        if (err) {
+            console.log('error turning off', err);
+            return;
+        }
+    });
+
+    beamer4.powerOff(function (err) {
+        if (err) {
+            console.log('error turning off', err);
+            return;
+        }
+    });
+
+    beamer5.powerOff(function (err) {
+        if (err) {
+            console.log('error turning off', err);
+            return;
+        }
+    });
+
+    console.log('Shutting down Projectors');
+}
+
+
+function shutdownProjectors(req, res) {
+
+    beamer1.powerOff(function (err) {
+        if (err) {
+            console.log('error turning off', err);
+            return;
+        }
+    });
+
+    beamer2.powerOff(function (err) {
+        if (err) {
+            console.log('error turning off', err);
+            return;
+        }
+    });
+
+    beamer3.powerOff(function (err) {
+        if (err) {
+            console.log('error turning off', err);
+            return;
+        }
+    });
+
+    beamer4.powerOff(function (err) {
+        if (err) {
+            console.log('error turning off', err);
+            return;
+        }
+    });
+
+    beamer5.powerOff(function (err) {
+        if (err) {
+            console.log('error turning off', err);
+            return;
+        }
+    });
+
+}
+
+//Timeline
+
+app.get('/product1', product1);
+app.get('/product2', product2);
+app.get('/product3', product3);
+app.get('/product4', product4);
+
+var product1 = Buffer.from("authenticate 1    \nreset \ndelay \"10\"  \nrun \"product1\"");
+var product2 = Buffer.from("authenticate 1    \nreset \ndelay \"10\"  \nrun \"product2\"");
+var product3 = Buffer.from("authenticate 1    \nreset \ndelay \"10\"  \nrun \"product3\"");
+var product4 = Buffer.from("authenticate 1    \nreset \ndelay \"10\"  \nrun \"product4\"");
+
+function product1(req, res) {
+    client.send(product1, 0, product1.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + HOST + ':' + PORT);
+        console.log(bytes);
+        //  client.close();
+        res.send('Playing Product1');
+    });
+}
+
+function product2(req, res) {
+    client.send(product2, 0, product2.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + HOST + ':' + PORT);
+        console.log(bytes);
+        //  client.close();
+        res.send('Playing Product2');
+    });
+}
+
+function product3(req, res) {
+    client.send(product3, 0, product3.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + HOST + ':' + PORT);
+        console.log(bytes);
+        //  client.close();
+        res.send('Playing Product3');
+    });
+}
+
+function product4(req, res) {
+    client.send(product4, 0, product4.length, PORT, HOST, function (err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + HOST + ':' + PORT);
+        console.log(bytes);
+        //  client.close();
+        res.send('Playing Product4');
+    });
+}
